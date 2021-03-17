dd5ee8bd92d26955b36b22741f6eccb137f41786
jakub_02 <baransky@spse-po.sk>
2021-03-17 21:32:10 +0100

    funkcny sound button, info button, logo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+
+import java.io.File;
+
 
 public class Main extends Application {
 
@@ -15,7 +21,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
