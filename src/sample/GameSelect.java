a2bd1bbb58d46214e5bad550f3a0604cb8088b8e
jakub_02 <baransky@spse-po.sk>
2021-03-22 13:31:52 +0100

    back button

--- src/sample/GameSelect.java
+++ src/sample/GameSelect.java
@@ -1,8 +1,24 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GameSelect {
-    public TextArea head;
 
+    public void back(MouseEvent mouseEvent) throws IOException {
+        Parent GameSelectWindow = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene GameSelectScene = new Scene(GameSelectWindow, 800, 600);
+
+        Stage windowShop = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+
+        windowShop.setScene(GameSelectScene);
+        windowShop.show();
+    }
 }
