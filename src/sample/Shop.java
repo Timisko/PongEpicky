f1e0aec6395f84eefd0e60cd3111d806549a5f64
SamuelHetes <hetes@spse-po.sk>
2021-04-27 20:27:18 +0200

    nakupovacia animacia, odcitanie penazi

--- src/sample/Shop.java
+++ src/sample/Shop.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Circle;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
@@ -31,6 +32,31 @@ public class Shop implements Initializable {
     public ImageView buy6;
     public Circle backgroundMask6;
     public Label money;
+    public ImageView tick1;
+    public ImageView dollar1;
+    public Label label1;
+    public int peniaze;
+    public int pomoc1;
+    public ImageView tick2;
+    public ImageView dollar2;
+    public Label label2;
+    public int pomoc2;
+    public ImageView dollar3;
+    public Label label3;
+    public ImageView tick3;
+    public int pomoc3;
+    public ImageView tick4;
+    public ImageView dollar4;
+    public Label label4;
+    public int pomoc4;
+    public ImageView tick5;
+    public ImageView dollar5;
+    public Label label5;
+    public int pomoc5;
+    public ImageView tick6;
+    public ImageView dollar6;
+    public Label label6;
+    public int pomoc6;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -43,7 +69,7 @@ public class Shop implements Initializable {
 
     public void load() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("peniaze.txt"));
-        int peniaze = Integer.parseInt(br.readLine());
+        peniaze = Integer.parseInt(br.readLine());
         money.setText(""+peniaze);
     }
 
@@ -144,4 +170,72 @@ public class Shop implements Initializable {
     }
 
 
+    public void buyButton1(MouseEvent mouseEvent) throws IOException {
+        dollar1.setVisible(false);
+        label1.setVisible(false);
+        tick1.setVisible(true);
+        backgroundMask1.setBackground(null);
+        buy1.setImage(null);
+        if (pomoc1 < 1) { peniaze -= 150;}
+        money.setText(""+peniaze);
+        pomoc1++;
+    }
+
+    public void buyButton2(MouseEvent mouseEvent) {
+        dollar2.setVisible(false);
+        label2.setVisible(false);
+        tick2.setVisible(true);
+        backgroundMask2.setBackground(null);
+        buy2.setImage(null);
+        if (pomoc2 < 1) { peniaze -= 150;}
+        money.setText(""+peniaze);
+        pomoc2++;
+    }
+
+    public void buyButton3(MouseEvent mouseEvent) {
+        dollar3.setVisible(false);
+        label3.setVisible(false);
+        tick3.setVisible(true);
+        backgroundMask3.setBackground(null);
+        buy3.setImage(null);
+        if (pomoc3 < 1) { peniaze -= 150;}
+        money.setText(""+peniaze);
+        pomoc3++;
+    }
+
+    public void buyButton4(MouseEvent mouseEvent) {
+        dollar4.setVisible(false);
+        label4.setVisible(false);
+        tick4.setVisible(true);
+        backgroundMask4.setFill(null);
+        backgroundMask4.setStroke(null);
+        buy4.setImage(null);
+        if (pomoc4 < 1) { peniaze -= 150;}
+        money.setText(""+peniaze);
+        pomoc4++;
+    }
+
+    public void buyButton5(MouseEvent mouseEvent) {
+        dollar5.setVisible(false);
+        label5.setVisible(false);
+        tick5.setVisible(true);
+        backgroundMask5.setFill(null);
+        backgroundMask5.setStroke(null);
+        buy5.setImage(null);
+        if (pomoc5 < 1) { peniaze -= 150;}
+        money.setText(""+peniaze);
+        pomoc5++;
+    }
+
+    public void buyButton6(MouseEvent mouseEvent) {
+        dollar6.setVisible(false);
+        label6.setVisible(false);
+        tick6.setVisible(true);
+        backgroundMask6.setFill(null);
+        backgroundMask6.setStroke(null);
+        buy6.setImage(null);
+        if (pomoc6 < 1) { peniaze -= 150;}
+        money.setText(""+peniaze);
+        pomoc6++;
+    }
 }
